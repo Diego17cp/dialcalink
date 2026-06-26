c52acb41a70d9213091fd45f29e8d093059ac215
Diego17 <diegoalbertocastropastor@gmail.com>
2026-06-25 22:25:05 -0500

    bootstrap device identity service in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notidialca/app/app.dart';
+import 'package:notidialca/core/identity/providers/device_identity_provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final container = ProviderContainer();
+  final identityService = await container.read(deviceIdentityServiceProvider.future);
+  await identityService.ensureBootstrapped();
   runApp(const ProviderScope(child: App()));
 }
