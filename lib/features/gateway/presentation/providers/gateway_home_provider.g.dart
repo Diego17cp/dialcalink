d33178e565e4610b5aaf233e956d257cc8c3b385
Diego17 <diegoalbertocastropastor@gmail.com>
2026-06-28 15:27:03 -0500

    implement gateway home provider with riverpod generator

---
+++ lib/features/gateway/presentation/providers/gateway_home_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gateway_home_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$gatewayHomeNotifierHash() =>
+    r'b321a2bc8707b9107b248e4d9a263e51da7b7ba8';
+
+/// See also [GatewayHomeNotifier].
+@ProviderFor(GatewayHomeNotifier)
+final gatewayHomeNotifierProvider =
+    AutoDisposeNotifierProvider<GatewayHomeNotifier, GatewayHomeState>.internal(
+      GatewayHomeNotifier.new,
+      name: r'gatewayHomeNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$gatewayHomeNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$GatewayHomeNotifier = AutoDisposeNotifier<GatewayHomeState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
