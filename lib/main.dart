26e114553ca0000b439566e8db1e54f3576d573b
Diego17 <diegoalbertocastropastor@gmail.com>
2026-06-30 20:19:37 -0500

    move gatewayServiceEntrypoint to main.dart and delete main_gateway_service.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-export 'package:notidialca/gateway_entrypoint/main_gateway_service.dart' show gatewayServiceEntrypoint;
+// export 'package:notidialca/gateway_entrypoint/main_gateway_service.dart' show gatewayServiceEntrypoint;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notidialca/app/app.dart';
 import 'package:notidialca/core/identity/providers/device_identity_provider.dart';
+import 'package:notidialca/features/gateway/presentation/providers/gateway_service_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,4 +11,18 @@ void main() async {
   final identityService = await container.read(deviceIdentityServiceProvider.future);
   await identityService.ensureBootstrapped();
   runApp(const ProviderScope(child: App()));
+}
+
+@pragma('vm:entry-point')
+void gatewayServiceEntrypoint() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final container = ProviderContainer();
+  container.read(gatewayServiceProvider.future).then(
+    (gatewayService) {
+      gatewayService.start().catchError((Object error, StackTrace stack) {
+      });
+    },
+    onError: (Object error, StackTrace stack) {
+    },
+  );
 }
