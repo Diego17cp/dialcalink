33aef46b7523d743018b9a678eb00941c9edffe8
Diego17 <diegoalbertocastropastor@gmail.com>
2026-07-03 10:19:36 -0500

    initialize App widget with router and theme provider integration

---
+++ lib/app/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notidialca/app/router/app_router.dart';
+import 'package:notidialca/app/theme/providers/theme_provider.dart';
+import 'package:notidialca/app/theme/theme.dart';
+
+class App extends ConsumerStatefulWidget {
+  const App({super.key});
+
+  @override
+  ConsumerState<App> createState() => _AppState();
+}
+class _AppState extends ConsumerState<App> {
+  @override
+  Widget build(BuildContext context) {
+    final themeAsync = ref.watch(themeControllerProvider);
+    final router = ref.watch(appRouterProvider);
+    return MaterialApp.router(
+      title: 'DialcaLink',
+      themeMode: themeAsync.when(
+        data: (mode) => mode,
+        loading: () => ThemeMode.system,
+        error: (_, __) => ThemeMode.system,
+      ),
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      routerConfig: router,
+    );
+  }
+}
