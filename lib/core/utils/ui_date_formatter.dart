cf735de3155a999f518e1954915580c399ef5f35
Diego17 <diegoalbertocastropastor@gmail.com>
2026-07-02 23:59:03 -0500

    add ui_date_formatter utility for relative time formatting

---
+++ lib/core/utils/ui_date_formatter.dart
@@ -0,0 +1,23 @@
+import 'package:intl/intl.dart';
+
+String formatRelativeTime(DateTime? dateTime) {
+  if (dateTime == null) return '';
+  final now = DateTime.now();
+  final difference = now.difference(dateTime);
+
+  if (difference.inMinutes < 1) {
+    return 'Ahora';
+  } else if (difference.inMinutes < 60) {
+    return 'Hace ${difference.inMinutes} m';
+  } else if (difference.inHours < 24) {
+    final startOfToday = DateTime(now.year, now.month, now.day);
+    if (dateTime.isAfter(startOfToday)) {
+      return DateFormat('h:mm a').format(dateTime);
+    }
+    return 'Ayer';
+  } else if (difference.inDays < 7) {
+    return DateFormat('EEEE').format(dateTime);
+  } else {
+    return DateFormat('MMM d').format(dateTime);
+  }
+}
